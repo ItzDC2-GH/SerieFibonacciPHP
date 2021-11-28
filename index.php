13980ea4af566d3716b3fffa8d1a77beb4f8fe5e
ItzDCraft <donovancf12380@gmail.com>
2021-11-28 23:05:59 +0000

    Version final

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    $numero = $_REQUEST["num"];
+    $numero = $_REQUEST["maximo"]; //Se pide el input por teclado.
 
-    if($numero != null) {
-        $fibonacci = [0,1];
-        for($i = 0; $i <= $numero; $i++) {
+    if($numero != null) { //Chequea si el input no es nulo
+        $fibonacci = [0,1]; //Establece el array de dos argumentos
+        for($i = 2; $i <= $numero; $i++) { //Se crea el ciclo que establezca el máximo 
             $fibonacci[] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+            echo "Iteración número: ". $i. ": ". $fibonacci[$i] . "<br>";
         }
-        echo $fibonacci[$numero];
     }
 ?>
--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function requerido() {
     if(maximo.value.length == 0) {
         alert("Necesitas poner un máximo");
         return false;
-    } else if(maximo.value.length > 0 && isNaN(maximo)) {
-        alert("Tiene que elegir un número");
-        return false;
-    } 
+    } else {
+        return true;
+    }
 }
